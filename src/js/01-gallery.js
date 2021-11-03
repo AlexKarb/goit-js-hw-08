d65342ddf1e598e2b0074eae77dac986817764d8
AlexKarb <alex.workinfo97@gmail.com>
2021-11-03 22:41:28 +0200

    add task 1&2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+const galleryRef = document.querySelector(".gallery")
+createGallery(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery__link', { captionDelay: 250 });
+
+
+function createGallery(array) {
+
+    const markup = array.map(el =>
+       `
+            <a class="gallery__link" href="${el.original}">
+                <img
+                class="gallery__image"
+                src="${el.preview}"
+                alt="${el.description}"
+                title="${el.description}"
+                />
+            </a>
+       `).join("");
+
+    return galleryRef.innerHTML = markup;
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import VimeoPlayer from '@vimeo/player';
+import { throttle } from 'lodash';    
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+  
+const updateCurrentTimeOfVideo = (time) => localStorage.setItem("videoplayer-current-time", time);
+const currentTimeofVideo = localStorage.getItem("videoplayer-current-time");
+
+
+
+player.on('timeupdate', throttle(() => {
+    return player.getCurrentTime().then(function (seconds) {
+
+        updateCurrentTimeOfVideo(seconds)
+        
+    }).catch(function (error) {
+        // an error occurred
+        console.log(error);
+        updateCurrentTimeOfVideo(0);
+    })
+}, 1000));
+
+
+
+player.setCurrentTime(currentTimeofVideo).then(function(seconds) {
+
+        updateCurrentTimeOfVideo(seconds);
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            updateCurrentTimeOfVideo(0);
+            break;
+
+        default:
+            // some other error occurred
+            updateCurrentTimeOfVideo(0);
+            break;
+    }
+});
