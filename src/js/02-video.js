7316b6d4462c8b75b037201fef2ec4b0ddd6e640
AlexKarb <alex.workinfo97@gmail.com>
2021-11-08 15:22:05 +0200

    return file 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,12 +12,32 @@ const currentTimeofVideo = localStorage.getItem("videoplayer-current-time");
 player.on('timeupdate', throttle(() => {
 
     return player.getCurrentTime().then(function (seconds) {
-
-        updateCurrentTimeOfVideo(seconds)
+            
+        updateCurrentTimeOfVideo(0)
         
+    }).catch(function (error) {
+        // an error occurred
+        updateCurrentTimeOfVideo(0);
     })
+   
 }, 1000));
 
 
 
-player.setCurrentTime(currentTimeofVideo);
+player.setCurrentTime(currentTimeofVideo).then(function(seconds) {
+
+        updateCurrentTimeOfVideo(seconds);
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            updateCurrentTimeOfVideo(0);
+            break;
+
+        default:
+            // some other error occurred
+            updateCurrentTimeOfVideo(0);
+            break;
+    }
+}); 
